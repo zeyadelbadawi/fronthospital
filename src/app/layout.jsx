13a7faf890309d4777543dc6ecd369ba2e6aa214
ZiadElbadawi <zeyadelbadawi.ze@gmail.com>
2025-07-14 01:41:43 +0300

    for save

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <PluginInit />
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
