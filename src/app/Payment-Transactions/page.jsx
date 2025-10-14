32ff6a2c87e6a08298b60ed474fba124846e1e40
ZiadElbadawi <zeyadelbadawi.ze@gmail.com>
2025-10-14 15:45:44 +0300

    Update page.jsx

--- src/app/Payment-Transactions/page.jsx
+++ src/app/Payment-Transactions/page.jsx
@@ -1,22 +1,22 @@
+"use client"
+import RBACWrapper from "@/components/RBACWrapper"
 import PaymentTransactionsTable from "@/components/PaymentTransactionsTable"
-import Breadcrumb from "@/components/Breadcrumb";
-import MasterLayout from "@/masterLayout/MasterLayout";
+import Breadcrumb from "@/components/Breadcrumb"
+import MasterLayout from "@/masterLayout/MasterLayout"
 
-
-const Page = () => {
+function PaymentTransactionsContent() {
   return (
-    <>
-      {/* MasterLayout */}
-      <MasterLayout>
-        {/* Breadcrumb */}
-        <Breadcrumb 
-  heading="Payment Managment System" 
-  title="Payment Managment System" 
-/>
-        <PaymentTransactionsTable />
-      </MasterLayout>
-    </>
-  );
-};
+    <MasterLayout>
+      <Breadcrumb heading="Payment Managment System" title="Payment Managment System" />
+      <PaymentTransactionsTable />
+    </MasterLayout>
+  )
+}
 
-export default Page;
+export default function PaymentTransactionsPage() {
+  return (
+    <RBACWrapper>
+      <PaymentTransactionsContent />
+    </RBACWrapper>
+  )
+}
