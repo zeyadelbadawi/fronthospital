b3b21f7bc1c49ae335ff4870d7d6afdd55ad805b
ZiadElbadawi <zeyadelbadawi.ze@gmail.com>
2025-10-14 15:45:23 +0300

    Update page.jsx

--- src/app/full-program-appointments/page.jsx
+++ src/app/full-program-appointments/page.jsx
@@ -1,29 +1,25 @@
-import AppointmentPage from "@/app/appointments/appointment-page";
-import Breadcrumb from "@/components/Breadcrumb";
-import MasterLayout from "@/masterLayout/MasterLayout";
-import { ModelProvider } from "@/contexts/ModelContext";
+"use client"
+import RBACWrapper from "@/components/RBACWrapper"
+import AppointmentPage from "@/app/appointments/appointment-page"
+import Breadcrumb from "@/components/Breadcrumb"
+import MasterLayout from "@/masterLayout/MasterLayout"
+import { ModelProvider } from "@/contexts/ModelContext"
 
+function FullProgramAppointmentsContent() {
+  return (
+    <MasterLayout>
+      <Breadcrumb heading="Full Program Appointments" title="Full Program Appointments" />
+      <ModelProvider>
+        <AppointmentPage />
+      </ModelProvider>
+    </MasterLayout>
+  )
+}
 
-
-const Page = () => {
-    return (
-        <>
-            {/* MasterLayout */}
-            <MasterLayout>
-                {/* Breadcrumb */}
-                <Breadcrumb
-                    heading="Full Program Appointments"
-                    title="Full Program Appointments"
-                />
-                <ModelProvider>
-
-                    <AppointmentPage />
-
-                </ModelProvider>
-
-            </MasterLayout>
-        </>
-    );
-};
-
-export default Page;
+export default function FullProgramAppointmentsPage() {
+  return (
+    <RBACWrapper>
+      <FullProgramAppointmentsContent />
+    </RBACWrapper>
+  )
+}
