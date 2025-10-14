25f386e8340b5ede70aea0526d6c207322452954
ZiadElbadawi <zeyadelbadawi.ze@gmail.com>
2025-10-14 15:32:35 +0300

    Update page.jsx

--- src/app/[role]/edit/[id]/page.jsx
+++ src/app/[role]/edit/[id]/page.jsx
@@ -1,32 +1,31 @@
-import GenericUserForm from "@/components/GenericUserForm"
+"use client"
+
+import { use } from "react"
+import RBACWrapper from "@/components/RBACWrapper"
 import Breadcrumb from "@/components/Breadcrumb"
 import MasterLayout from "@/masterLayout/MasterLayout"
+import GenericUserForm from "@/components/GenericUserForm"
 
-export async function generateMetadata({ params }) {
-  const awaitedParams = await params
-  const role = awaitedParams.role.replace(/-/g, " ").replace(/\b\w/g, (char) => char.toUpperCase())
-  return {
-    title: `Rukn Alwatikon Center - Edit ${role}`,
-    description: `Edit ${role} profile.`,
-  }
-}
-
-const EditPage = async ({ params }) => {
-  // Make the component async
-  const awaitedParams = await params // Await params
-  const role = awaitedParams.role // e.g., 'student', 'doctor', 'accountant'
-  const id = awaitedParams.id
+function EditPageContent({ role, id }) {
   const heading = `Edit ${role.charAt(0).toUpperCase() + role.slice(1)}`
   const title = `Edit ${role.charAt(0).toUpperCase() + role.slice(1)}`
 
   return (
-    <>
-      <MasterLayout>
-        <Breadcrumb heading={heading} title={title} />
-        <GenericUserForm role={role} mode="edit" id={id} />
-      </MasterLayout>
-    </>
+    <MasterLayout>
+      <Breadcrumb heading={heading} title={title} />
+      <GenericUserForm role={role} mode="edit" id={id} />
+    </MasterLayout>
   )
 }
 
-export default EditPage
+export default function EditPage({ params }) {
+  const awaitedParams = use(params)
+  const role = awaitedParams.role
+  const id = awaitedParams.id
+
+  return (
+    <RBACWrapper>
+      <EditPageContent role={role} id={id} />
+    </RBACWrapper>
+  )
+}
