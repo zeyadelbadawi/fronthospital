88e0d75d97a9ab463727869a1c08818df70e6f47
ZiadElbadawi <zeyadelbadawi.ze@gmail.com>
2025-10-14 15:24:33 +0300

    Update page.jsx

--- src/app/[role]/add/page.jsx
+++ src/app/[role]/add/page.jsx
@@ -1,6 +1,4 @@
-import GenericUserForm from "@/components/GenericUserForm"
-import Breadcrumb from "@/components/Breadcrumb"
-import MasterLayout from "@/masterLayout/MasterLayout"
+import AddPageClientWrapper from "./client-page"
 
 export async function generateMetadata({ params }) {
   const awaitedParams = await params
@@ -11,21 +9,6 @@ export async function generateMetadata({ params }) {
   }
 }
 
-const AddPage = async ({ params }) => {
-  // Make the component async
-  const awaitedParams = await params // Await params
-  const role = awaitedParams.role // e.g., 'student', 'doctor', 'accountant'
-  const heading = `Add ${role.charAt(0).toUpperCase() + role.slice(1)}`
-  const title = `Add ${role.charAt(0).toUpperCase() + role.slice(1)}`
-
-  return (
-    <>
-      <MasterLayout>
-        <Breadcrumb heading={heading} title={title} />
-        <GenericUserForm role={role} mode="add" />
-      </MasterLayout>
-    </>
-  )
+export default function AddPage({ params }) {
+  return <AddPageClientWrapper params={params} />
 }
-
-export default AddPage
