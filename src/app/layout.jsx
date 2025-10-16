dca7681a3644ad2e08bc0b5acf9d60f9aad927c0
ZiadElbadawi <zeyadelbadawi.ze@gmail.com>
2025-10-16 12:24:46 +0300

    Add cookies banner

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,12 @@
-import { LanguageProvider } from "../contexts/LanguageContext"
+import { LanguageProvider } from "@/contexts/LanguageContext"
+import { CookieConsentProvider } from "@/contexts/CookieConsentContext"
+import CookieConsentBanner from "@/components/CookieConsentBanner"
 import { registerLicense } from "@syncfusion/ej2-base"
 import { ToastContainer } from "react-toastify"
-import { LoadingProvider } from "../contexts/LoadingContext"
-import LoadingSpinner from "../components/LoadingSpinner"
-import NProgressBar from "../components/NProgressBar" // Import NProgressBar
-import "../styles/nprogress.css"
+import { LoadingProvider } from "@/contexts/LoadingContext"
+import LoadingSpinner from "@/components/LoadingSpinner"
+import NProgressBar from "@/components/NProgressBar"
+import "@/styles/nprogress.css"
 
 registerLicense(process.env.NEXT_PUBLIC_SYNCFUSION_LICENSE_KEY)
 
@@ -18,36 +20,37 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Preload critical resources for better performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        {/* DNS prefetch for better loading performance */}
         <link rel="dns-prefetch" href="//fonts.googleapis.com" />
         <link rel="dns-prefetch" href="//fonts.gstatic.com" />
       </head>
       <body suppressHydrationWarning={true}>
-        <NProgressBar /> {/* NProgressBar is now a standalone component */}
-        <LanguageProvider>
-          <LoadingProvider>
-            <LoadingSpinner />
-            {children}
-          </LoadingProvider>
-          <ToastContainer
-            position="top-right"
-            autoClose={4000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-            toastClassName="custom-toast"
-            bodyClassName="custom-toast-body"
-            progressClassName="custom-toast-progress"
-          />
-        </LanguageProvider>
+        <NProgressBar />
+        <CookieConsentProvider>
+          <LanguageProvider>
+            <LoadingProvider>
+              <LoadingSpinner />
+              {children}
+            </LoadingProvider>
+            <ToastContainer
+              position="top-right"
+              autoClose={4000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+              toastClassName="custom-toast"
+              bodyClassName="custom-toast-body"
+              progressClassName="custom-toast-progress"
+            />
+          </LanguageProvider>
+          <CookieConsentBanner />
+        </CookieConsentProvider>
       </body>
     </html>
   )
