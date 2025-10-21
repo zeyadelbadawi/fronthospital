dfb79bb8fb8a50de5fa941fc7d59db0925775c11
ZiadElbadawi <zeyadelbadawi.ze@gmail.com>
2025-10-21 16:37:47 +0300

    Update main-content.jsx

--- src/app/school/components/main-content.jsx
+++ src/app/school/components/main-content.jsx
@@ -1,8 +1,9 @@
 "use client"
 
-import { SidebarInset, SidebarTrigger } from "./ui/sidebar"
+import { SidebarInset } from "./ui/sidebar"
 import { useContentStore } from "../store/content-store"
 import { SchoolAppointments } from "./school-appointments"
+import { SchoolAppointmentsAssignment } from "./school-appointments-assignment"
 
 import AllPatientsSchool from "./all-patients-school"
 import styles from "../styles/main-content.module.css"
@@ -17,27 +18,29 @@ export function MainContent() {
 
     const { department, type } = activeContent
 
-
- if (department === "school-up") {
+    if (department === "school-up") {
       if (type === "appointments") {
-         return <SchoolAppointments />
-       }
-     }
+        return <SchoolAppointments />
+      }
+    }
 
+    if (department === "school-assign") {
+      if (type === "assign-doctors") {
+        return <SchoolAppointmentsAssignment />
+      }
+    }
 
     if (department === "school-p") {
-     if (type === "patients") {
+      if (type === "patients") {
         return <AllPatientsSchool />
       }
     }
 
-   
     return <SchoolAppointments />
   }
 
   return (
     <SidebarInset className={styles.mainContent}>
-      
       <main className={styles.mainBody}>{renderContent()}</main>
     </SidebarInset>
   )
